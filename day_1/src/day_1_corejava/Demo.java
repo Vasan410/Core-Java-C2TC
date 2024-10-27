07a3161ff9fd7416e13658d96aa431e4a9ca174f
Vasan410 <msdhoni.keerthi4102004@gmail.com>
2024-10-27 22:24:27 +0530

    This is my java basic project

---
+++ day_1/src/day_1_corejava/Demo.java
@@ -0,0 +1,54 @@
+//Printing part
+/*
+package day_1_corejava;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+
+	}
+
+}
+*/
+//Basic addition program
+/*
+package day_1_java;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value for a & b: ");
+		a = s.nextInt();
+		b = s.nextInt();
+		c = a + b;
+		System.out.println(+a+" + " +b+" = "+c);
+
+	}
+
+}*/
+
+/*
+package day_1_corejava;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		int a,b;
+		double c;
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value for a & b: ");
+		a = s.nextInt();
+		b = s.nextInt();
+		c = a + b;
+		System.out.println( "c = " +c);
+
+	}
+
+}
+*/
